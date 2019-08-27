04f1d8ad3008bf9687c964aa0ebc7c7dea43b8a5
Michal Nagosa <michal.nagosa@safe-t.com>
2019-08-27 19:55:06 +0300

    final2

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise2 here
+sudo su
+nano /etc/hosts
+sed -i '/www/d' hosts
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sudo su
+ssh-keygen -t rsa -b 4096
+ls ~/.ssh/id_*
+ssh-copy-id root@server1
+vagrant
+ssh root@server1
